680bb45c654c55a753616632eaaa014c82c3cb2d
rleira <rleira@rlmac15.local>
2019-12-16 20:53:31 -0300

    Adding logic to collect creeps

---
+++ Main.gd
@@ -0,0 +1,66 @@
+extends Node
+
+export (PackedScene) var Mob
+
+const LEVEL_MAX_TIME = 30
+
+var score
+var time
+
+func _ready():
+	randomize()
+	newGame()
+
+func gameOver():
+	$LevelTimer.stop()
+	$MobTimer.stop()
+	$HUD.showGameOver()
+	$Music.stop()
+	$DeathSound.play()
+	$HUD.updateTime(LEVEL_MAX_TIME)
+
+func newGame():
+	score = 0
+	time = LEVEL_MAX_TIME
+	$HUD.updateTime(LEVEL_MAX_TIME)
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.updateScore(score)
+	$HUD.showMessage("Get Ready")
+	$HUD.hideStartButton()
+	$Music.play()
+
+func _on_MobTimer_timeout():
+	# Choose a random location on Path2D.
+	$MobPath/MobSpawnLocation.set_offset(randi())
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
+	add_child(mob)
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# Set the mob's position to a random location.
+	mob.position = $MobPath/MobSpawnLocation.position
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	
+	# Connect delete of mobs at game end
+	$HUD.connect("start_game", mob, "_on_start_game")
+
+func _on_LevelTimer_timeout():
+	time -= 1
+	$HUD.updateTime(time)
+	if time <= 0:
+		gameOver()
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$LevelTimer.start()
+
+func _on_Player_hit():
+	if time > 0:
+		score += 1
+		$HUD.updateScore(score)
---
+++ hud/HUD.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+signal start_game
+
+func showMessage(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+func showGameOver():
+	showMessage("Game Over")
+	yield($MessageTimer, "timeout")
+	$MessageLabel.text = "Collect the\nCreeps!"
+	$MessageLabel.show()
+	# TODO move next line to a metod
+	yield(get_tree().create_timer(1), 'timeout')
+	$StartButton.show()
+
+func updateScore(score):
+	$ScoreLabel.text = str(score)
+
+func updateTime(time):
+	$TimeLabel.text = str(time)
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
+
+func _on_StartButton_pressed():
+	hideStartButton()
+	emit_signal("start_game")
+	
+func hideStartButton():
+	$StartButton.hide()
