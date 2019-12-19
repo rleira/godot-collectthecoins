b40f223f202a47d90d7a8174f55904ffe8ff01a7
rleira <rleira@rlmac15.local>
2019-12-18 21:31:44 -0300

    Renamed references to mob to coin

--- Main.gd
+++ Main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-export (PackedScene) var Mob
+export (PackedScene) var Coin
 
 const LEVEL_MAX_TIME = 30
 
@@ -32,23 +32,23 @@ func newGame():
 
 func _on_CoinTimer_timeout():
 	# Choose a random location on Path2D.
-	$MobPath/MobSpawnLocation.set_offset(randi())
-	# Create a Mob instance and add it to the scene.
-	var mob = Mob.instance()
-	add_child(mob)
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
-	# Set the mob's position to a random location.
-	mob.position = $MobPath/MobSpawnLocation.position
+	$CoinPath/CoinSpawnLocation.set_offset(randi())
+	# Create a Coin instance and add it to the scene.
+	var coin = Coin.instance()
+	add_child(coin)
+	# Set the coin's direction perpendicular to the path direction.
+	var direction = $CoinPath/CoinSpawnLocation.rotation + PI / 2
+	# Set the coin's position to a random location.
+	coin.position = $CoinPath/CoinSpawnLocation.position
 	# Add some randomness to the direction.
 	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+	coin.rotation = direction
 	# Set the velocity (speed & direction).
-	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
-	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	coin.linear_velocity = Vector2(rand_range(coin.min_speed, coin.max_speed), 0)
+	coin.linear_velocity = coin.linear_velocity.rotated(direction)
 	
-	# Connect delete of mobs at game end
-	$HUD.connect("start_game", mob, "_on_start_game")
+	# Connect delete of coins at game end
+	$HUD.connect("start_game", coin, "_on_start_game")
 
 func _on_LevelTimer_timeout():
 	time -= 1
